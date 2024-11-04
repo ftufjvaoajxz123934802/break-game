19a5aeebeafc1811c5ddff71510151dcd893cecc
qwerfdsa <albert.yu9765@gmail.com>
2024-11-04 11:52:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -41,8 +41,7 @@ function checkSpecialNumber() {
 
 function backToGame(){
     document.getElementById('intro').classList.add('hidden');
-        
-document.getElementById('level2').classList.remove('hidden');
+    document.getElementById('level2').classList.remove('hidden');
     setTimeout(() => {
         window.location.href = 'index.html'; 
     }, 200); 
