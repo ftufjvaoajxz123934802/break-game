aef7faeb6c124c93776741fc28381d2a566f8a2c
qwerfdsa <albert.yu9765@gmail.com>
2024-11-04 12:18:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function checkSpecialNumber() {
 }
 
 function backToGame(){
-    document.getElementById('intro').classList.add('hidden');
+    
     
 document.getElementById('easter-01').classList.add('hidden');
     document.getElementById('level2').classList.remove('hidden');
